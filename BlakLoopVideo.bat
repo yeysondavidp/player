966362db69cb24ccfd7f564d5cd3c0e7d319a479
yeysondavidp <38136724+yeysondavidp@users.noreply.github.com>
2023-11-22 12:17:51 +1100

    Update BlakLoopVideo.bat
    using the same folder

--- BlakLoopVideo.bat
+++ BlakLoopVideo.bat
@@ -1,6 +1,5 @@
 @echo off
 
-copy video.mp4 C:\Users\BLAK-NAVORI\Videos\video.mp4
-start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show "C:\Users\BLAK-NAVORI\Videos\video.mp4"
+start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show video.mp4
 
 exit
