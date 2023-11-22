f952de9ee50ecde5989a0253f60e4b7a30890231
yeysondavidp <38136724+yeysondavidp@users.noreply.github.com>
2023-11-22 12:13:55 +1100

    Test1

--- BlakLoopVideo.bat
+++ BlakLoopVideo.bat
@@ -1,5 +1,6 @@
 @echo off
 
-start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show "C:\Videos\video.mp4"
+copy video.mp4 C:\Users\BLAK-NAVORI\Videos\video.mp4
+start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show "C:\Users\BLAK-NAVORI\Videos\video.mp4"
 
 exit
