10797375f55e3ba97e9ed335551a2b5d4dbf191f
yeysondavidp <38136724+yeysondavidp@users.noreply.github.com>
2023-11-22 11:53:24 +1100

    Update BlakLoopVideo.bat
    Change in video route and name

--- BlakLoopVideo.bat
+++ BlakLoopVideo.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show "C:\Users\BLAK-NAVORI\Videos\EPISODE-3-ENG.mp4"
+start /min "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --loop --no-video-title-show "C:\Videos\video.mp4"
 
 exit
